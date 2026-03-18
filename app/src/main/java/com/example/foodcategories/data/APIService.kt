f18165742f3a58eedbf04601cec6e8f15e5679d8
idiot-Dev <3amkhero@gmail.com>
2026-03-18 19:26:51 +0200

    fix UI and API problems

--- app/src/main/java/com/example/foodcategories/data/APIService.kt
+++ app/src/main/java/com/example/foodcategories/data/APIService.kt
@@ -1,9 +1,10 @@
 package com.example.foodcategories.data
 
+import retrofit2.Call
 import retrofit2.http.GET
 
 interface APIService {
     //www.themealdb.com/api/json/v1/1/categories.php
-    @GET("categories")
-    fun listAll(): List<Categoty>
+    @GET("categories.php")
+    fun listAll(): Call<Model>
 }
--- app/src/main/java/com/example/foodcategories/ui/MainActivity.kt
+++ app/src/main/java/com/example/foodcategories/ui/MainActivity.kt
@@ -1,14 +1,57 @@
 package com.example.foodcategories.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.foodcategories.R
+import com.example.foodcategories.data.APIService
+import com.example.foodcategories.data.Model
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    lateinit var recyclerView: RecyclerView
+    lateinit var adapter: CategoryAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        adapter = CategoryAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        fetchCategories()
+    }
+
+    fun fetchCategories(){
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://www.themealdb.com/api/json/v1/1/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val apiService = retrofit.create(APIService::class.java)
+
+        apiService.listAll().enqueue(object : Callback<Model> {
+            override fun onResponse(call: Call<Model>, response: Response<Model>) {
+                if (response.isSuccessful) {
+                    val categories = response.body()?.categories
+                    adapter.submitList(categories)
+                }
+            }
+
+            override fun onFailure(call: Call<Model>, t: Throwable) {
+                Log.e("MainActivity", "Error: ${t.message}")
+            }
+        })
+
     }
 }
